fccae76b8142437d94d7eff90a7352f87e1b4ee9
Swaggus <89348630+Swagguss@users.noreply.github.com>
2023-04-03 19:44:50 -0700

    Add project files.

---
+++ Playlister/PlayLister.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Playlister
+{
+    partial class PlayLister
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PlayLister));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(12, 411);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(750, 45);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 48);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem3});
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem2.Text = "File";
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem3.Text = "Load Folder";
+            // 
+            // PlayLister
+            // 
+            this.ClientSize = new System.Drawing.Size(1078, 468);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Name = "PlayLister";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadFolderToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private AxWMPLib.AxWindowsMediaPlayer audioPlayer;
+        private System.Windows.Forms.ListBox Playlist;
+        private System.Windows.Forms.Label FileName;
+        private System.Windows.Forms.Label lblDuration;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.ListBox listBox1;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+    }
+}
+
---
+++ Playlister/PlayLister.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+
+namespace Playlister
+{
+    public partial class PlayLister : Form
+    {
+
+        List<string> filteredFiles = new List<string>();
+        FolderBrowserDialog browser = new FolderBrowserDialog();
+        int currentFile = 0;
+
+        string prevText = "";
+
+        public PlayLister()
+        {
+            InitializeComponent();
+        }
+
+        private void PlayLister_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e) // file menu event
+        {
+
+        }
+
+        private void loadFolderToolStripMenuItem_Click(object sender, EventArgs e) // Load Folder Event
+        {
+            audioPlayer.Ctlcontrols.stop();
+
+            if (filteredFiles.Count > 1)
+            {
+                filteredFiles.Clear();
+                filteredFiles = null;
+
+                Playlist.Items.Clear();
+                currentFile = 0;
+            }
+
+            DialogResult result = browser.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                filteredFiles = Directory.GetFiles(browser.SelectedPath, "*.*").Where(file => file.ToLower().EndsWith("mp3") || file.ToLower().EndsWith("wav")).ToList();
+
+                LoadPlaylist();
+            }
+        }
+
+        private void optionsToolStripMenuItem_Click(object sender, EventArgs e) // options menu event
+        {
+
+        }
+
+        private void MediaPlayerStateChangedEvent(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            if (e.newState == 0)
+            {
+                lblDuration.Text = "Loaded";
+            } else if (e.newState == 1)
+            {
+                lblDuration.Text = "Player stopped";
+            } else if (e.newState == 3)
+            {
+                lblDuration.Text = "Duration: " + audioPlayer.currentMedia.durationString;
+            } else if (e.newState == 8)
+            {
+                int lastFile = currentFile;
+                if (currentFile >= filteredFiles.Count - 1)
+                {
+                    currentFile = 0;
+                } else
+                {
+                    Random rand = new Random();
+
+                    int tryrand = rand.Next(filteredFiles.Count);
+
+                    if (tryrand > filteredFiles.Count)
+                    {
+                        currentFile = tryrand;
+                    } else
+                    {
+                        tryrand = rand.Next(filteredFiles.Count);
+                    }
+                }
+
+                Playlist.SelectedIndex = currentFile;
+                ShowFileName(FileName);
+            } else if (e.newState == 9)
+            {
+                lblDuration.Text = "Loading...";
+            } else if (e.newState == 10)
+            {
+                timer1.Start();
+            }
+        }
+
+        private void PlaylistChanged(object sender, EventArgs e)
+        {
+            currentFile = Playlist.SelectedIndex;
+            PlayFile(Playlist.SelectedItem.ToString());
+            ShowFileName(FileName);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TimerEvent(object sender, EventArgs e)
+        {
+            audioPlayer.Ctlcontrols.play();
+            timer1.Stop();
+        }
+
+        private void LoadPlaylist()
+        {
+            audioPlayer.currentPlaylist = audioPlayer.newPlaylist("Playlist", "");
+
+            foreach (string audio in filteredFiles)
+            {
+                audioPlayer.currentPlaylist.appendItem(audioPlayer.newMedia(audio));
+                Playlist.Items.Add(audio);
+            }
+
+            if (filteredFiles.Count > 0)
+            {
+                FileName.Text = "Files Found: " + filteredFiles.Count;
+
+                Playlist.SelectedIndex = currentFile;
+
+                PlayFile(Playlist.SelectedItem.ToString());
+            } else
+            {
+                MessageBox.Show("No Audio files found in this folder.");
+            }
+        }
+
+        private void PlayFile(string url)
+        {
+            audioPlayer.URL = url;
+        }
+
+        private void ShowFileName(Label name)
+        {
+            string file = Path.GetFileName(Playlist.SelectedItem.ToString());
+            name.Text = "Currently Playing: " + file;
+        }
+    }  
+}
